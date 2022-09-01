ec0f9e4e7ca2c34a37ea987a0c5c2779242601d9
Ben <benjaminrbarnes@gmail.com>
2022-08-31 21:13:29 -0400

    Update contact.jsx

--- src/views/contact.jsx
+++ src/views/contact.jsx
@@ -2,7 +2,9 @@ import { Outlet, Link } from "react-router-dom";
 export default function One() {
   return (
     <main>
-      <p>benjaminrbarnes@gmail.com</p>
+      <p><b>Email</b> - benjaminrbarnes@gmail.com<br/>
+      <b>Instagram</b> - <a href="https://www.instagram.com/benjaminbarnes/">@benjaminbarnes</a>
+      </p>
     </main>
   );
 }
